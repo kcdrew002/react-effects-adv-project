f9d54bd31c21b27e9b1ce65bcf5b73703712a8ba
Kevin Drew <kcdrew002@gmail.com>
2025-06-30 12:29:48 -0400

    Logic for completed quiz

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import completedImg from "../assets/quiz-complete.png"
+import QUESTIONS from "../questions";
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  const quizComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  if (quizComplete){
+    return <div id="summary">
+        <img src={completedImg} alt="Trophy icon"/>
+        <h2>Quiz Completed</h2>
+    </div>
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
